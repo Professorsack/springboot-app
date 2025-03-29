7cb994a2c946c8a04e8cfb8f0e1b9fd718ee1a6c
professorsack <joseph0206chan@gmail.com>
2025-03-29 19:11:06 -0400

    Testing auto-deploy

--- src/main/java/com/professorsack/first/controller/UserController.java
+++ src/main/java/com/professorsack/first/controller/UserController.java
@@ -17,7 +17,7 @@ public class UserController {
         this.userService = userService;
     }
 
-    //comment for testing 2
+    //comment for testing 3
     @GetMapping
     public List<User> getUsers() {
         return userService.getAllUsers();
